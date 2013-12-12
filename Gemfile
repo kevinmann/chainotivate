620ea7e914f0e3fb0ad5aef101e5ebf6215eb71a
Kevin Mann <kevin@audacious.me>
2013-12-12 16:21:47 +0000

    Set up devise for users

--- Gemfile
+++ Gemfile
@@ -27,6 +27,8 @@ gem 'haml'
 
 gem "haml-rails"
 
+gem "devise"
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Chain::Application.routes.draw do
+  # get "users/show"
+
+  # get "home/index"
+
+  root :to => "home#index"
+
+  devise_for :users
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
