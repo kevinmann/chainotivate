a0ba49ae804d0b6037e14a9991831b57e8ae00d3
Kevin Mann <kevin@audacious.me>
2013-12-12 15:09:22 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chain::Application.config.secret_token = '094552b53db384e7ef299dfbe6d10983f48aa132c36cfc47a479ef9c6924d448d146c673135f985f0fe227e5e397a82ef31498164e55eb11ab6b4ca7a3aa54a2'
